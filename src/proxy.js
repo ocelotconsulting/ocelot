0031c61fc6982f2c71e704cf5ce543105a738423
CJCOFF <christopher.j.coffman@monsanto.com>
2015-07-27 11:01:09 -0500

    fixing bug, should use path instead of pathname

--- src/proxy.js
+++ src/proxy.js
@@ -1,7 +1,7 @@
 var Promise = require('promise');
 
 exports.request = function(proxy, req, res, url) {
-  req.url = url.pathname;
+  req.url = url.path;
 
   proxy.web(req, res, {
     target: url.protocol + "//" + url.host
