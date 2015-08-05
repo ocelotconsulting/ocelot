7e0e8ca8186712c3f588e34eec9313f123360f61
CJCOFF <christopher.j.coffman@monsanto.com>
2015-08-05 13:03:43 -0500

    tests for resolver

--- src/resolver.js
+++ src/resolver.js
@@ -1,35 +1,41 @@
 var http = require('http'),
-  cache = require('./metadata/cache.js'),
-  uri = require('url'),
-  _ = require("underscore");
+    cache = require('./metadata/cache'),
+    uri = require('url'),
+    _ = require("underscore");
 
 cache.initCache();
 
 function findClosestRoute(url) {
-  var path = uri.parse(url).pathname;
-  if (path.indexOf('/') === 0) {
-    path = path.substring(1);
-  }
-  for (pathDepth = 3; pathDepth > 0; pathDepth--) {
-    var routePath = path.split('/', pathDepth).join('/');
-    var foundRoute = _.find(cache.getRoutes(), function(route) {
-      return route.route === routePath;
-    });
-    if (typeof foundRoute !== "undefined") {
-      return foundRoute;
+    var path = getUrlStrStripLeadingSlash(url);
+    for (pathDepth = 3; pathDepth > 0; pathDepth--) {
+        var routePath = path.split('/', pathDepth).join('/');
+        var foundRoute = _.find(cache.getRoutes(), function (route) {
+            return route.route === routePath;
+        });
+        if (typeof foundRoute !== "undefined") {
+            return foundRoute;
+        }
     }
-  }
-  console.log("no matching service found for " + url);
-  return null;
+    console.log("no matching service found for " + url);
+    return null;
 }
 
-exports.resolveRoute = function(url) {
-  var closestRoute = findClosestRoute(url);
-  if (closestRoute != null) {
-    closestRoute['instances'] = {};
-    _.each(closestRoute.services, function(service) {
-      closestRoute['instances'][service] = cache.getServices()[service];
-    });
-  }
-  return closestRoute;
+function getUrlStrStripLeadingSlash(urlStr) {
+    var path = uri.parse(urlStr).pathname;
+    if (path.indexOf('/') === 0) {
+        path = path.substring(1);
+    }
+    return path;
+}
+
+exports.resolveRoute = function (url) {
+    var closestRoute = findClosestRoute(url);
+    if (closestRoute != null) {
+        closestRoute['instances'] = {};
+        _.each(closestRoute.services, function (service) {
+            console.log("<<<<<< " + service);
+            closestRoute['instances'][service] = cache.getServices()[service];
+        });
+    }
+    return closestRoute;
 };
---
+++ test/resolver-spec.js
@@ -0,0 +1,86 @@
+var assert = require('assert'),
+    sinon = require('sinon'),
+    cache = require('../src/metadata/cache'),
+    resolver = require('../src/resolver');
+
+describe('resolver', function () {
+
+    afterEach(function () {
+        restore(cache.getRoutes);
+        restore(cache.getServices);
+    });
+
+    it('return null if cannot find route', function () {
+        sinon.stub(cache, "getRoutes", function () {
+            return [{}];
+        });
+
+        assert.equal(resolver.resolveRoute("http://monsanto.com/abc"), null);
+    });
+
+    it('returns route if found, level 1', function () {
+        sinon.stub(cache, "getRoutes", function () {
+            return [{route: 'abc'}];
+        });
+
+        assert.equal(resolver.resolveRoute("http://monsanto.com/abc").route, 'abc');
+    });
+
+    it('returns route if found, level 2', function () {
+        sinon.stub(cache, "getRoutes", function () {
+            return [{route: 'abc/def'}];
+        });
+
+        assert.equal(resolver.resolveRoute("http://monsanto.com/abc/def").route, 'abc/def');
+    });
+
+    it('returns route if found, level 3', function () {
+        sinon.stub(cache, "getRoutes", function () {
+            return [{route: 'abc/def/ghi'}];
+        });
+
+        assert.equal(resolver.resolveRoute("http://monsanto.com/abc/def/ghi").route, 'abc/def/ghi');
+    });
+
+    it('does not go to level 4', function () {
+        sinon.stub(cache, "getRoutes", function () {
+            return [{route: 'abc/def/ghi/jkl'}];
+        });
+
+        assert.equal(resolver.resolveRoute("http://monsanto.com/abc/def/ghi/jkl"), null);
+    });
+
+    it('uses closest path', function () {
+        sinon.stub(cache, "getRoutes", function () {
+            return [{route: 'abc/def'}];
+        });
+
+        assert.equal(resolver.resolveRoute("http://monsanto.com/abc/def/ghi").route, 'abc/def');
+    });
+
+    it('returns whole cache object', function () {
+        sinon.stub(cache, "getRoutes", function () {
+            return [{route: 'abc/def', fruit: "banana"}];
+        });
+
+        assert.equal(resolver.resolveRoute("http://monsanto.com/abc/def/ghi").fruit, 'banana');
+    });
+
+    it('adds service urls', function () {
+        sinon.stub(cache, "getRoutes", function () {
+            return [{route: 'abc/def', services: ['service1']}];
+        });
+
+        sinon.stub(cache, "getServices", function () {
+            return {service1: [{url: "www.monsanto.com"}]};
+        });
+
+        assert.equal(resolver.resolveRoute("http://monsanto.com/abc/def/ghi").instances.service1[0].url, 'www.monsanto.com');
+    });
+
+    function restore(mockFunc) {
+        if (mockFunc.restore) {
+            mockFunc.restore();
+        }
+    }
+});
